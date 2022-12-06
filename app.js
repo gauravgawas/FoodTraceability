85cadf06afa7c90979b2c628fb9a8f44345953e4
Gaurav Gawas <gauravgawas9999@gmail.com>
2022-12-06 19:45:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,133 @@
+var contractAbi = [
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "index",
+				"type": "uint256"
+			}
+		],
+		"name": "Added",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_productId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "info",
+				"type": "string"
+			}
+		],
+		"name": "addState",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_a",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_b",
+				"type": "string"
+			}
+		],
+		"name": "concat",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_text",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_exp",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_det",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_prc",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_ls",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_pc",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_date",
+				"type": "string"
+			}
+		],
+		"name": "newItem",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_productId",
+				"type": "uint256"
+			}
+		],
+		"name": "searchProduct",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+];
+
+var contractAddress ='0xeE6078424c19955E8Aa6e8863467138b413dB832';
---
+++ smartcontract/smartcontract.sol
@@ -0,0 +1,81 @@
+pragma solidity ^0.6.0;
+
+contract SupplyChain {
+    
+    event Added(uint256 index);
+    
+    struct State{
+        string description;
+        address person;
+    }
+    
+    struct Product{
+        address creator;
+        string productName;
+        string productExp;
+        uint256 productId;
+        string date;
+        string productdetail;
+        string price;
+        string lotsize;
+        uint256 totalStates;
+        mapping (uint256 => State) positions;
+    }
+    
+    mapping(uint => Product) allProducts;
+    uint256 items=0;
+    
+    function concat(string memory _a, string memory _b) public returns (string memory){
+        bytes memory bytes_a = bytes(_a);
+        bytes memory bytes_b = bytes(_b);
+        string memory length_ab = new string(bytes_a.length + bytes_b.length);
+        bytes memory bytes_c = bytes(length_ab);
+        uint k = 0;
+        for (uint i = 0; i < bytes_a.length; i++) bytes_c[k++] = bytes_a[i];
+        for (uint i = 0; i < bytes_b.length; i++) bytes_c[k++] = bytes_b[i];
+        return string(bytes_c);
+    }
+    
+    function newItem(string memory _text,string memory _det,string memory _prc,string memory _exp,string memory _ls, string memory _date) public returns (bool) {
+        Product memory newItem = Product({creator: msg.sender, totalStates: 0,productName: _text ,productdetail: _det ,price: _prc ,productExp: _exp ,lotsize: _ls , productId: items, date: _date});
+        allProducts[items]=newItem;
+        items = items+1;
+        emit Added(items-1);
+        return true;
+    }
+    
+    function addState(uint _productId, string memory info) public returns (string memory) {
+        require(_productId<=items);
+        
+        State memory newState = State({person: msg.sender, description: info});
+        
+        allProducts[_productId].positions[ allProducts[_productId].totalStates ]=newState;
+        
+        allProducts[_productId].totalStates = allProducts[_productId].totalStates +1;
+        return info;
+    }
+    
+    function searchProduct(uint _productId) public returns (string memory) {
+
+        require(_productId<=items);
+        string memory output="Product Name: ";
+        output=concat(output, allProducts[_productId].productName);
+        output=concat(output, "<br>Manufacture Date: ");
+        output=concat(output, allProducts[_productId].date);
+        output=concat(output, "<br>Product Details: ");
+        output=concat(output, allProducts[_productId].productdetail);
+        output=concat(output, "<br>Price / Unit: ");
+        output=concat(output, allProducts[_productId].price);
+        output=concat(output, "<br>Product Expiry: ");
+        output=concat(output, allProducts[_productId].productExp);
+        output=concat(output, "<br>Lot Size: ");
+        output=concat(output, allProducts[_productId].lotsize);
+        
+        for (uint256 j=0; j<allProducts[_productId].totalStates; j++){
+            output=concat(output, allProducts[_productId].positions[j].description);
+        }
+        return output;
+        
+    }
+    
+}
